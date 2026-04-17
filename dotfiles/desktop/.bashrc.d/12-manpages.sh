271173662379caa1b22aacc2b66ed493f90a7464
Fabio Scotto di Santolo <fscottodev@pm.me>
2026-04-17 16:17:49 +0200

    Colorize man pages through LESS_TERMCAP

---
+++ dotfiles/desktop/.bashrc.d/12-manpages.sh
@@ -0,0 +1,10 @@
+export LESS='-R'
+
+# Keep man pages readable with a subdued Yaru-like palette.
+export LESS_TERMCAP_mb=$'\e[0;38;5;110m'
+export LESS_TERMCAP_md=$'\e[1;38;5;111m'
+export LESS_TERMCAP_me=$'\e[0m'
+export LESS_TERMCAP_so=$'\e[38;5;252;48;5;238m'
+export LESS_TERMCAP_se=$'\e[0m'
+export LESS_TERMCAP_us=$'\e[0;38;5;209m'
+export LESS_TERMCAP_ue=$'\e[0m'

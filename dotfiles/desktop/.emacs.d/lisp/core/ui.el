dbe135cbdbcad0695012a176fd9d5ae2d22a69ed
Fabio Scotto di Santolo <fscottodev@pm.me>
2026-05-15 22:18:31 +0200

    desktop: unify mono font to LiterationMono Nerd Font, add alacritty dotfiles

--- dotfiles/desktop/.emacs.d/lisp/core/ui.el
+++ dotfiles/desktop/.emacs.d/lisp/core/ui.el
@@ -7,10 +7,10 @@
   (load-theme 'nordic-night t))
 
 ;; Setting default font
-(set-frame-font "Liberation Mono 14" nil t)
+(set-frame-font "LiterationMono Nerd Font 14" nil t)
 
 (add-to-list 'default-frame-alist
-	     '(font . "Liberation Mono-14"))
+	     '(font . "FantasqueSansM Nerd Font-14"))
 
 ;; Remove toolbar
 (tool-bar-mode -1)
